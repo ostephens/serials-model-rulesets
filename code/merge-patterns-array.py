f36b22248ef6c7f857f8a12f5b36fec17b28933e
Owen Stephens <owen@ostephens.com>
2025-02-13 17:08:45 +0000

    Minor code changes

--- code/merge-patterns-array.py
+++ code/merge-patterns-array.py
@@ -4,16 +4,17 @@ import glob
 
 def merge_json_files(directory_path):
     merged_data = []
-    file_paths = glob.glob(directory_path + '/sg-*.json')
+    file_paths = glob.glob(directory_path + '/*.json')
     for path in file_paths:
         with open(path, 'r') as file:
+            print(path)
             data = {}
             data['ruleset'] = json.load(file)
             merged_data.append(data)
     return merged_data
 
 
-directory_path = "./"
+directory_path = "../generic-patterns"
 output_file = "merged.json"
 merged_data = merge_json_files(directory_path)
 with open(output_file, 'w') as outfile:
