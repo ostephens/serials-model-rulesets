a2d7421cb7ed7a4d36cbe87a9d6bd3dc2f0bb22d
Owen Stephens <owen@ostephens.com>
2025-02-25 10:14:29 +0000

    V basic python script to create an array of rulesets from individual files

--- code/merge-patterns-array.py
+++ code/merge-patterns-array.py
@@ -6,15 +6,14 @@ def merge_json_files(directory_path):
     merged_data = []
     file_paths = glob.glob(directory_path + '/*.json')
     for path in file_paths:
+        print(path)
         with open(path, 'r') as file:
-            print(path)
-            data = {}
-            data['ruleset'] = json.load(file)
-            merged_data.append(data)
+            merged_data.append(json.load(file))
     return merged_data
 
 
-directory_path = "../generic-patterns"
+#directory_path = "../generic-patterns"
+directory_path = "../ruleset-files/GBV"
 output_file = "merged.json"
 merged_data = merge_json_files(directory_path)
 with open(output_file, 'w') as outfile:
